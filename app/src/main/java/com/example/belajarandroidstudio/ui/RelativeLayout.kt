7f5e5a0c1bd3ad1e86437cd0f78168417b91aa2e
SindhuPaw <sindhupawitra@gmail.com>
2023-09-14 18:42:46 +0700

    init

---
+++ app/src/main/java/com/example/belajarandroidstudio/ui/RelativeLayout.kt
@@ -0,0 +1,12 @@
+package com.example.belajarandroidstudio.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.belajarandroidstudio.R
+
+class RelativeLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative_layout)
+    }
+}
